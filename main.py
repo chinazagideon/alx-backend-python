6ee924a520e1d9775fb5f772a19341bbc5471055
chinazagideon <chinazagideon264@gmail.com>
2025-07-05 17:59:24 +0100

    update readme, add main.py latest

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import importlib.util
 
+#THIS IMPLEMENTATION IS ADAPTED TO BATCH PROCESSING
+
 # Import the module properly
 spec = importlib.util.spec_from_file_location("batch_processing", "python-generators-0x00/1-batch_processing.py")
 processing = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(processing)
+
 
 ##### print processed users in a batch of 50
 try:
