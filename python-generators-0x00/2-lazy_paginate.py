e98144a3c118380251d6211764471f898059c603
chinazagideon <chinazagideon264@gmail.com>
2025-07-05 17:25:45 +0100

    add paginate_user function

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -2,16 +2,20 @@ import seed as seed
 from itertools import islice
 
 # Lazy Pagination Generator with offset and limit
-def lazy_paginate(limit=10, offset=0):
+def lazy_paginate(page_size=10, offset=0):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM user_data LIMIT {limit} OFFSET {offset}")
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     rows = cursor.fetchall()
     cursor.close()
     connection.close()
     return rows
 
+def paginate_users(page_size=10, offset=0):
+    for user in islice(lazy_paginate(page_size, offset), page_size):
+        yield user
+
 # Main execution
 if __name__ == "__main__":
-    for user in islice(lazy_paginate(10, 0), 10):
+    for user in islice(paginate_users(10, 0), 10):
         print(user)
