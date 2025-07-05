94bbc29b55c2c23bbdcc9e8288ae3a8342779ef8
chinazagideon <chinazagideon264@gmail.com>
2025-07-05 16:41:40 +0100

    Task 3: batch processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,23 @@
+from itertools import islice
+import seed as seed
+connection = seed.connect_to_prodev()
+
+def batch_processing(batch_size=10):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM user_data WHERE age > 25 LIMIT {batch_size}")
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
+
+def stream_users_in_batches(batch_size=10):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size}")
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
+
+if __name__ == "__main__":
+    for user in islice(batch_processing(10), 10):
+        print(user)
