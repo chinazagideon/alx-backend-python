cb434793c0419423d02bd72dab9aaac82af61f45
chinazagideon <chinazagideon264@gmail.com>
2025-07-05 16:58:09 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import importlib.util
+
+# Import the module properly
+spec = importlib.util.spec_from_file_location("batch_processing", "python-generators-0x00/1-batch_processing.py")
+processing = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(processing)
+
+##### print processed users in a batch of 50
+try:
+    for user in processing.batch_processing(50):
+        print(user)
+except BrokenPipeError:
+    sys.stderr.close()
