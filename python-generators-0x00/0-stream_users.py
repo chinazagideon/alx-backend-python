d4936386e193c52311c55c742a776daad1db50aa
chinazagideon <chinazagideon264@gmail.com>
2025-07-05 16:16:19 +0100

    use generator to stream users

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,19 @@
+from itertools import islice, chain
+import seed as seed
+connection = seed.connect_to_prodev()
+
+def stream_users(limit=10):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM user_data LIMIT {limit}")
+    for row in cursor:
+        yield row
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    total_users = cursor.fetchone()[0]
+    print(f"Total users: {total_users}")
+    cursor.close()
+    connection.close()
+
+if __name__ == "__main__":
+    for user in islice(stream_users(10), 10):
+        print(user)
+       
