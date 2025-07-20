04cedf21b9ead5700363599a4c53fdac1cc0d37f
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 16:54:28 +0100

    add urlpattern

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include chats app URLs
-    path('', include('chats.urls')),
+    path('api/', include('chats.urls')),
 ]
