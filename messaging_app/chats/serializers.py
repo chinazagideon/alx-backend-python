59cae1d33f028e00ea2fc4d1dd96e7e9ebfe5fb9
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 15:27:05 +0100

    serialize  message body

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -24,13 +24,15 @@ class ConversationSerializer(serializers.ModelSerializer):
     Serializer for Conversation objects.
     Handles nested messages and custom fields.
     """
-    
+    # this field is not inherited from the AbstractConversation class
     perticipants = serializers.PrimaryKeyRelatedField(
         queryset=settings.AUTH_USER_MODEL.objects.all(), 
         many=True,
         help_text=("IDs of users participating in this conversation")
         )
+    # this field is not inherited from the AbstractConversation class
     perticipants_count = serializers.SerializerMethodField()
+    # this field is not inherited from the AbstractConversation class
     last_message_preview = serializers.SerializerMethodField()
 
     class Meta:
@@ -42,13 +44,20 @@ class MessageSerializer(serializers.ModelSerializer):
     """
     Serializer for the Message model
     """
+    # this field is not inherited from the AbstractMessage class
+    conversation_id = serializers.PrimaryKeyRelatedField(
+        queryset=settings.AUTH_CONVERSATION_MODEL.objects.all(),
+        help_text=("The conversation this message belongs to")
+    )
+    # this field is not inherited from the AbstractMessage class
+    message_body = serializers.CharField(
+        max_length=1000,
+        min_length=1,
+        help_text=("The content of the message")
+    )
 
-    class Meta:
-        model = settings.AUTH_MESSAGE_MODEL
-        fields = "__all__"
-        read_only_fields = ["message_id", "conversation_id"]
-        
-    def validate_content(self, value):
+    # this field is not inherited from the AbstractMessage class
+    def validate_message_body(self, value):
         """
         Validate the content of the message
         """
@@ -57,6 +66,12 @@ class MessageSerializer(serializers.ModelSerializer):
         if len(value) > 1000:
             raise serializers.ValidationError("Message content is too long")
         return value
+    
+    # this field is not inherited from the AbstractMessage class
+    class Meta:
+        model = settings.AUTH_MESSAGE_MODEL
+        fields = "__all__"
+        read_only_fields = ["message_id", "conversation_id"]
 
 class ChatSerializer(serializers.ModelSerializer):
     """
