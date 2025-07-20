b4986c7a22ab92c91a3a127dd8ef192810423f9a
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 15:31:36 +0100

    define chat views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,80 @@
+# chats/views.py
+"""
+This file contains the views for the chats app
+"""
+
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User, Conversation, Message, Chat
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer, ChatSerializer
 
 # Create your views here.
+
+def filter_by_user(queryset, user_id):
+    """
+    Filter the queryset by the user id
+    """
+    return queryset.filter(user_id=user_id)
+
+def filter_by_conversation(queryset, conversation_id):
+    """
+    Filter the queryset by the conversation id
+    """
+    return queryset.filter(conversation_id=conversation_id)
+
+def filter_by_status(queryset, status):
+    """
+    Filter the queryset by the status
+    """
+    return queryset.filter(status=status)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        """
+        Filter the queryset by the user id
+        """
+        return filter_by_user(self.queryset, self.request.user.id)
+    
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows conversations to be viewed or edited.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        """
+        Filter the queryset by the conversation id
+        """
+        return filter_by_conversation(self.queryset, self.request.user.id)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows messages to be viewed or edited.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Filter the queryset by the status
+        """
+        return filter_by_status(self.queryset, self.request.user.id)
+
+class ChatViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows chats to be viewed or edited.
+    """
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    
