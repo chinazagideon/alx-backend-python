d1f027db14784243f3c45cdc4fabd9cb687ad865
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 16:56:42 +0100

    add auth route

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include chats app URLs
     path('api/', include('chats.urls')),
+    path('api/auth/', auth_views.obtain_auth_token),
 ]
