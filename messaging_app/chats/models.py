fc207cf591da2b1d15b127aad293a7e01c0f1493
chinazagideon <chinazagideon264@gmail.com>
2025-07-20 14:51:02 +0100

    override the user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,9 +4,61 @@ This file contains the models for the chats app
 """
 
 from django.db import models
-from user.models import User
+from django.contrib.auth.models import AbstractUser
 from message.models import Message
 from uuid import uuid4
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    """
+    This model is used to store the user details
+    """
+    # this fields are inherited from the AbstractUser class
+    user_id = models.UUIDField(
+        primary_key=True, 
+        default=uuid4, 
+        editable=False,
+        help_text=_("User\'s unique identifier")
+    )
+    first_name = models.CharField(
+        _("first name"),
+        max_length=255,
+        null=False,
+        blank=False,
+        help_text=_("User\'s first name")
+    )
+
+    last_name = models.CharField(
+        _("last name"),
+        max_length=255,
+        null=False,
+        blank=False,
+        help_text=_("User\'s last name")
+    )
+    email = models.EmailField(
+        _("email address"),
+        max_length=255,
+        null=False,
+        blank=False,
+        unique=True,
+        help_text=_("User\'s email address")
+    )
+    password = models.CharField(
+        _("password"),
+        max_length=255,
+        null=False,
+        blank=False,
+        help_text=_("User\'s password")
+    )
+    phone_number = models.CharField(
+        _("phone number"),
+        max_length=15, 
+        null=False, 
+        blank=False,
+        unique=True,
+        help_text=_("User\'s phone number, e.g., +12125551212")
+    )
 
 # Create your models here.
 class Chat(models.Model):
