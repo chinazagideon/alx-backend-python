46802e68ff54f16f361344a834dfcf34ed0ae4a4
chinazagideon <chinazagideon264@gmail.com>
2025-07-26 01:13:57 +0100

    implement get_paginated_response

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,15 +1,26 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from collections import OrderedDict
 
 class CustomPagination(PageNumberPagination):
     """
-    Custom pagination class to set a default page size for messages.
-    This class is used by Django REST Framework to paginate querysets.
-    When applied, a 'page' object will have attributes like 'paginator.count'.
+    Custom pagination class to set a default page size for messages,
+    inheriting from BasePagination and explicitly setting the count.
     """
     
     page_size = 20
-    page_size_query_param='page_size'
-    max_page_size=100
+    # page_size_query_param='page_size'
+    # max_page_size=100
     
-    _test_hook = "This pagination class works with a paginator.count attribute."
-
+    def get_paginated_response(self, data):
+        """
+        wrap response with a paginated response
+        """
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data 
+        })
